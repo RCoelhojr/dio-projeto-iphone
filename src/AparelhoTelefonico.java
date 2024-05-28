9938301487d9e646519e85059b386f9648560491
RCoelhojr <ramiltoncoelho@gmail.com>
2024-05-27 21:20:32 -0300

    iPhone

--- src/AparelhoTelefonico.java
+++ src/AparelhoTelefonico.java
@@ -1,5 +1,9 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+
+ 
+ public interface AparelhoTelefonico {
+
+ public void ligar(String numero);
+ public void atender(String numero);
+ public void iniciarCorreioVoz(String numero);
 }
+
---
+++ src/NavegadorInternet.java
@@ -0,0 +1,9 @@
+
+
+public interface NavegadorInternet {
+
+ public void exibirPagina(String url);
+ public void novaAba(String url);
+ public void atualizarPagina();
+
+}
